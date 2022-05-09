629101bc6148ca041db478b1fbf0116339586a6f
Guillaume Roig <groig64@gmail.com>
2022-05-09 21:39:14 +0200

    [200~Faire le lien entre un produit de la page

---
+++ front/js/product.js
@@ -0,0 +1,30 @@
+let str = window.location.href;
+let url = new URL(str);
+let idProduct = url.searchParams.get("id");
+console.log(idProduct);
+let article = "";
+
+const colorPicked = document. querySelector("#colors");
+const quantityPicked = document.querySelector("#quantity");
+
+getArticle();
+
+// Récupération des articles de l'API
+function getArticle() {
+    fetch("http://localhost:3000/api/products/" + idProduct)
+    .then((res) => {
+        return res.json();
+    })
+
+    // Répartition des données de l'API dans le DOM
+    .then(async function (resultatAPI) {
+        article = await resultatAPI;
+        console.table(article);
+        if (article){
+            getPost(article);
+        }
+    })
+    .catch((error) => {
+        console.log("Erreur de la requête API");
+    })
+}
