4997cc6082bdd46b201b4395782666776e98ba49
Guillaume Roig <groig64@gmail.com>
2022-06-07 20:47:00 +0200

    Récupération de l'id depuis l'api pour créer chaque page des canapés.

--- front/js/product.js
+++ front/js/product.js
@@ -6,3 +6,51 @@ let url = new URL(str);
 let idCouch = URLSearchParams.get("id");
 console.log(idCouch);
 
+// Récupération des informations des produits dans l'API
+
+async function getCouchDataList() {
+    let couchDataList = await fetch("http://localhost:3000/api/products" + idCouch);
+    return await couchDataList.json();
+}
+
+// A partir des éléments de mon API, je vais créer le contenu des pages pour mes différents canapés. (Img, titre, prix...)
+
+async function pageContent() {
+    let couchDataList = await getCouchDataList();
+    couchDataList.forEach(couch => {
+
+// Ajout de l'image
+
+let couchImage = document.createElement("img");
+couchImage.src = couch.imageUrl;
+couchImage.alt = couch.altTxt;
+document.querySelector("#item__img").appendChild(couchImage);
+
+// Création du titre "h1"
+
+let couchTitle = document.getElementById('title');
+couchTitle.innerHTML = couch.name;
+
+// Ajout du prix
+
+let couchPrice = document.getElementById('price');
+couchPrice.innerHTML = couch.price;
+
+// Création de la description
+
+let couchDescription = document.getElementById('description');
+couchDescription.innerHTML = couch.description;
+
+// Ajout des options de couleurs
+
+for (let colors of couch.colors){
+    console.table(colors);
+    let couchColors = document.createElement("option");
+    couchColors.value = colors;
+    couchColors.innerHTML = colors;
+    document.querySelector("#colors").appendChild(couchColors);
+}
+})
+}
+
+pageContent();
